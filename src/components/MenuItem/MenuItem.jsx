4f45c0bfcfb3be7cbbe5d56cb88d761b30d6b5be
jmekstrom <jmekstrom@gmail.com>
2020-10-06 13:19:25 -0700

    update our MenuItems to navigate withRouter HOC

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; //higher order component, a fucntion that take a component as an arguement and returns a modified componenet
 import './MenuItem.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`}>
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
+    <div 
+        className={`menu-item ${size}`} 
+        onClick={()=>history.push(`${match.url}${linkUrl}`)}
+    >
         <img className="background-image" src={imageUrl} alt={title}/>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
@@ -11,4 +15,5 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem) // now MenuItem has access to location, match and history props from routing,
+                                    // this prevents prop tunnelling or passing props through a parent that doesn't need it just to pass it to the child;
